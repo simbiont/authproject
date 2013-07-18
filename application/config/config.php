e0aa2650609fa88663edad8c3ce3438707ec6e7a
dspirin <dmiriy.spirin@gmail.com>
2013-07-18 19:44:58 +0300

    up

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://23.23.179.157/authproject.dev/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
